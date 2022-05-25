317cc3a6d7e157b77a9d82950fb6fdb2068c90b0
Yura <bolotinya@mail.ru>
2022-05-25 19:48:43 +0300

    Домашнее задание листы и сеты

---
+++ src/main/java/com/example/workhome25/WorkHome25Application.java
@@ -0,0 +1,13 @@
+package com.example.workhome25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHome25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHome25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workhome25/WorkHome25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workhome25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHome25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
